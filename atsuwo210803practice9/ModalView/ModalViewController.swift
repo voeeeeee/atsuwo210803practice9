81c52eedc0d50fb29e81523663cdd01e2d708769
voeeeeee <at.0130.145@gmail.com>
2021-08-04 00:35:02 +0900

    作成途中

---
+++ atsuwo210803practice9/ModalView/ModalViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ModalViewController.swift
+//  atsuwo210803practice9
+//
+//  Created by 竹辻篤志 on 2021/08/03.
+//
+
+import UIKit
+
+class ModalViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationItem.backBarButtonItem = UIBarButtonItem(title: "戻る", style: .plain, target: nil, action: nil)
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- atsuwo210803practice9/ViewController.swift
+++ atsuwo210803practice9/ViewController.swift
@@ -7,13 +7,22 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIViewControllerTransitioningDelegate {
 
+    @IBOutlet weak var prefactureLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
     }
 
+    @IBAction func tappedModal(_ sender: Any) {
+        let vc = self.storyboard?.instantiateViewController(withIdentifier: "ModalViewController") as! ModalViewController
+//        let vc = ModalViewController()
+//        vc.modalPresentationStyle = .custom
+//        vc.transitioningDelegate = self
+        self.navigationController?.pushViewController(vc, animated: true)
+        print("aa")
+    }
 
 }
 
