65ce7b91f464161f6c4749d0725851e4c11366ad
voeeeeee <at.0130.145@gmail.com>
2021-08-03 22:49:33 +0900

    Initial Commit

---
+++ atsuwo210803practice9/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  atsuwo210803practice9
+//
+//  Created by 竹辻篤志 on 2021/08/03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
